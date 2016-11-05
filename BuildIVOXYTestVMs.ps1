94d428025167801738e94f444df79dc92e0cd9f0
Chris Crow <chris@ivoxy.com>
2016-11-04 17:30:17 -0700

    Still need to fix confirm, added DRS rule

--- BuildIVOXYTestVMs.ps1
+++ BuildIVOXYTestVMs.ps1
@@ -4,22 +4,22 @@ $viserver = "seadc1vc01.ldthost.pvt"
 connect-viserver -server $viserver
 
 $TemplateName = "SEADC1-W2012R2"
-$VMs = @("Ivoxy1")
+$VMs = @("seadc1-ivoxytest1","seadc1-ivoxytest2","seadc1-ivoxytest3","seadc1-ivoxytest4")
 $Custom = "IVOXY-Win-DHCP"
 $Portgroup = "VLAN48-ESXi-MGMT"
 $dvswitch = "DSwitche0"
 $folder = "Discovered Virtual Machine"
-$datastore = "GENERAL-SEADC1VC01CLST3"
-$ResourcePool = get-cluster -name "cluster3" | Get-ResourcePool
+$datastore = "POC-SEADC1VC01CLST1-AF"
+$ResourcePool = get-cluster -name "cluster1" | Get-ResourcePool
 
 
 
 
 foreach ($VM in $VMs) {
-    new-vm -name $VM -template $TemplateName -datastore $datastore   -ResourcePool $ResourcePool
-    get-vm -name $VM | get-networkadapter | Set-NetworkAdapter -NetworkName $Portgroup
-    New-AdvancedSetting -Entity $vm -Name ‘vcpu.hotadd’ -Value ‘true’ -Confirm:$false
-    New-AdvancedSetting -Entity $vm -Name ‘mem.hotadd’ -Value ‘true’ -Confirm:$false
+    new-vm -name $VM -template $TemplateName -datastore $datastore   -ResourcePool $ResourcePool -OSCustomizationSpec $Custom
+    get-vm -name $VM | get-networkadapter | Set-NetworkAdapter -NetworkName $Portgroup 
+    #New-AdvancedSetting -Entity $vm -Name ‘vcpu.hotadd’ -Value ‘true’ -Confirm:$false
+    #New-AdvancedSetting -Entity $vm -Name ‘mem.hotadd’ -Value ‘true’ -Confirm:$false
    
     
     
@@ -34,14 +34,12 @@ foreach ($VM in $VMs) {
     $disks = Get-Harddisk -VM $VM | Select -Last 2
     New-ScsiController -Type ParaVirtual -Harddisk $disks
 
-    get-vm -name $VM | set-vm -numcpu 4 -MemoryGB 8
-
-
-
-
+    get-vm -name $VM | set-vm -numcpu 4 -MemoryGB 8 
 
 
 }
+
+
 
 
 disconnect-viserver
